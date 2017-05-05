8d466bcb8d731184e5e9635c78cd022d510d75ef
André <andre.jalbut@gmail.com>
2017-05-05 13:55:59 -0300

    Primeira versão funcional apenas com backorder

--- src/kim/relogo/ChainLevel.groovy
+++ src/kim/relogo/ChainLevel.groovy
@@ -18,11 +18,9 @@ class ChainLevel extends ReLogoTurtle {
 	static float THETA = 0.5
 
 	float desiredStock = 20
-	float desiredSupplyLine = 60
-	float Q
 	float currentStock
 	float expectedDemand
-	
+
 	Map ordersToFulfill = [:]
 	Map ordersSent = [:]
 	Map shipmentsSent = [:]
@@ -33,6 +31,7 @@ class ChainLevel extends ReLogoTurtle {
 	Map lastShipmentsSent = [:]
 	Map lastProductPipelines = [:]
 	Map lastOrderPipelines = [:]
+	Map ordersReceived = [:]
 
 	def upstreamLevel
 	def downstreamLevel
@@ -41,37 +40,33 @@ class ChainLevel extends ReLogoTurtle {
 	def initialOrderPipeline = [4.0]
 
 	def setup(initialStock){
-		this.Q = this.desiredStock + this.BETA * this.desiredSupplyLine
 		this.currentStock = initialStock
 		this.expectedDemand = 12.0
 		for (ChainLevel upstream in this.upstreamLevel) {
 			this.lastOrdersSent[upstream.getWho()] = 4.0
-			this.lastProductPipelines[upstream.getWho()] = initialProductPipeline
+			this.lastProductPipelines[upstream.getWho()] = initialProductPipeline.clone()
 		}
 		for (ChainLevel downstream in this.downstreamLevel) {
 			this.lastOrdersToFulfill[downstream.getWho()] = 0.0
 			this.lastShipmentsSent[downstream.getWho()] = 4.0
-			this.lastOrderPipelines[downstream.getWho()] = initialOrderPipeline
+			this.lastOrderPipelines[downstream.getWho()] = initialOrderPipeline.clone()
 		}
 	}
 
 	def receiveShipments(){
-		this.productPipelines = this.lastProductPipelines
 		for (ChainLevel upstream in this.upstreamLevel) {
-			printf 'pipelineBefore' + this.productPipelines[upstream.getWho()] + '\n'
-			printf 'addedShipment' + upstream.lastShipmentsSent[this.getWho()] + '\n'
+			this.productPipelines[upstream.getWho()] = this.lastProductPipelines[upstream.getWho()].clone()
 			this.productPipelines[upstream.getWho()].add(0, upstream.lastShipmentsSent[this.getWho()])
-			printf 'pipelineMid' + this.productPipelines[upstream.getWho()] + '\n'
 			this.currentStock += this.productPipelines[upstream.getWho()].pop()
-			printf 'pipelineAfter' + this.productPipelines[upstream.getWho()] + '\n'
 		}
 	}
 
 	def receiveOrders(){
-		this.orderPipelines = this.lastOrderPipelines 
 		for (ChainLevel downstream in this.downstreamLevel) {
+			this.orderPipelines[downstream.getWho()] = this.lastOrderPipelines[downstream.getWho()].clone()
 			this.orderPipelines[downstream.getWho()].add(0, downstream.lastOrdersSent[this.getWho()])
-			this.ordersToFulfill[downstream.getWho()] = this.lastOrdersToFulfill[downstream.getWho()] + this.orderPipelines[downstream.getWho()].pop()
+			this.ordersReceived[downstream.getWho()] = this.orderPipelines[downstream.getWho()].pop()
+			this.ordersToFulfill[downstream.getWho()] = this.lastOrdersToFulfill[downstream.getWho()] + this.ordersReceived[downstream.getWho()]
 		}
 	}
 
@@ -80,13 +75,9 @@ class ChainLevel extends ReLogoTurtle {
 		for (ChainLevel downstream in this.downstreamLevel) {
 			totalOrdersToFulfill += this.ordersToFulfill[downstream.getWho()]
 		}
-		float totalShipmentsSent = (this.currentStock >= totalOrdersToFulfill) ? totalOrdersToFulfill : this.currentStock 
+		float totalShipmentsSent = (this.currentStock >= totalOrdersToFulfill) ? totalOrdersToFulfill : this.currentStock
 		for (ChainLevel downstream in this.downstreamLevel) {
-//			printf 'ordersToFulfillWho '+ this.ordersToFulfill[downstream.getWho()] + '-' + downstream.getWho() + '\n'
-//			printf 'currentStockWho '+ this.currentStock + '-' + this.getWho() + '\n'
-//			printf 'totalOrdersToFulfill '+ totalOrdersToFulfill + '\n'
 			this.shipmentsSent[downstream.getWho()] = totalOrdersToFulfill ? totalShipmentsSent * this.ordersToFulfill[downstream.getWho()] / totalOrdersToFulfill : 0.0
-//			printf 'shipmentsSentWho '+ this.shipmentsSent[downstream.getWho()] + '-' + downstream.getWho() + '\n'
 			this.currentStock -= this.shipmentsSent[downstream.getWho()]
 			this.ordersToFulfill[downstream.getWho()] -= this.shipmentsSent[downstream.getWho()]
 		}
@@ -98,10 +89,9 @@ class ChainLevel extends ReLogoTurtle {
 		float inverseBackOrderSum = 0.0
 		for (ChainLevel upstream in this.upstreamLevel) {
 			supplyLine += this.productPipelines[upstream.getWho()].sum()
-			if(upstream.getWho() != this.getWho()) { 
+			if(upstream.getWho() != this.getWho()) {
 				supplyLine += upstream.lastOrdersToFulfill[this.getWho()]
 				supplyLine += upstream.lastOrderPipelines[this.getWho()].sum()
-				printf 'upstreamOrdersThis' + upstream.lastOrdersToFulfill[this.getWho()]
 				if (!upstream.lastOrdersToFulfill[this.getWho()]) {
 					freeUpstreams.push(upstream.getWho())
 				} else {
@@ -109,29 +99,24 @@ class ChainLevel extends ReLogoTurtle {
 				}
 			}
 		}
-		
-		float totalShipmentsSent = 0.0
+
+		float totalOrdersReceived = 0.0
 		for (ChainLevel downstream in this.downstreamLevel) {
-			totalShipmentsSent += this.shipmentsSent[downstream.getWho()]
+			totalOrdersReceived += this.ordersReceived[downstream.getWho()]
 		}
-		
-		this.expectedDemand = this.THETA * totalShipmentsSent + (1 - this.THETA) * this.expectedDemand
-		float totalOrders = this.expectedDemand + this.ALPHA * (this.Q - this.currentStock - this.BETA * supplyLine)
+
+		this.expectedDemand = this.THETA * totalOrdersReceived + (1 - this.THETA) * this.expectedDemand
+		float desiredSupplyLine = 3 * this.expectedDemand
+		float Q = this.desiredStock + this.BETA * desiredSupplyLine
+		float totalOrders = this.expectedDemand + this.ALPHA * (Q - this.currentStock - this.BETA * supplyLine)
 		float totalOrdersSent =  Math.max(0, totalOrders)
-		
+
 		for (ChainLevel upstream in this.upstreamLevel) {
 			if(upstream.getWho() == this.getWho()) {
 				this.ordersSent[upstream.getWho()] = totalOrdersSent
 			} else {
 				if(length(freeUpstreams)) {
-//					printf 'supplyLine' + supplyLine + '\n'
-//					printf 'totalOrders' + totalOrders + '\n'
-//					printf 'totalOrdersSent' + totalOrdersSent + '\n'
-//					printf 'upstreamWho' + upstream.getWho() + '\n'
-//					printf 'freeUpstreams '+ freeUpstreams + '\n'
-//					printf 'testcontains '+ freeUpstreams.contains(upstream.getWho()) + '\n'
 					this.ordersSent[upstream.getWho()] = (freeUpstreams.contains(upstream.getWho())) ? totalOrdersSent / length(freeUpstreams) : 0.0
-//					printf 'ordersSentWho' + this.ordersSent[upstream.getWho()] + '\n'
 				} else {
 					this.ordersSent[upstream.getWho()] = totalOrdersSent * (1 / upstream.lastOrdersToFulfill[this.getWho()]) / inverseBackOrderSum
 				}
@@ -140,15 +125,14 @@ class ChainLevel extends ReLogoTurtle {
 	}
 
 	def updateState(){
-//		printf 'ordersToFulfill'+ this.ordersToFulfill + '\n'
-//		printf 'ordersSent'+ this.ordersSent + '\n'
-//		printf 'shipmentsSent'+ this.shipmentsSent + '\n'
-//		printf 'productPipelines'+ this.productPipelines + '\n'
-//		printf 'orderPipelines'+ this.orderPipelines + '\n\n'
-		this.lastOrdersToFulfill = this.ordersToFulfill
-		this.lastOrdersSent = this.ordersSent
-		this.lastShipmentsSent = this.shipmentsSent
-		this.lastProductPipelines = this.productPipelines
-		this.lastOrderPipelines = this.orderPipelines
+		for (ChainLevel downstream in this.downstreamLevel) {
+			this.lastOrdersToFulfill[downstream.getWho()] = this.ordersToFulfill[downstream.getWho()]
+			this.lastShipmentsSent[downstream.getWho()] = this.shipmentsSent[downstream.getWho()]
+			this.lastOrderPipelines[downstream.getWho()] = this.orderPipelines[downstream.getWho()].clone()
+		}
+		for (ChainLevel upstream in this.upstreamLevel) {
+			this.lastOrdersSent[upstream.getWho()] = this.ordersSent[upstream.getWho()]
+			this.lastProductPipelines[upstream.getWho()] = this.productPipelines[upstream.getWho()].clone()
+		}
 	}
 }
--- src/kim/relogo/Factory.groovy
+++ src/kim/relogo/Factory.groovy
@@ -14,15 +14,14 @@ import repast.simphony.relogo.schedule.Setup
 @Plural ("Factories")
 class Factory extends ChainLevel {
 	def setup(initialStock){
-		this.desiredSupplyLine = 12
-		this.initialProductPipeline = [4.0, 4.0, 4.0]
+		this.initialProductPipeline = [12.0, 12.0, 12.0]
 		this.upstreamLevel = [this]
 		this.downstreamLevel = distributors()
 		super.setup(initialStock)
 	}
 
 	def receiveShipments(){
-		this.productPipelines = this.lastProductPipelines
+		this.productPipelines[this.getWho()] = this.lastProductPipelines[this.getWho()].clone()
 		this.productPipelines[this.getWho()].add(0, this.lastOrdersSent[this.getWho()])
 		this.currentStock += this.productPipelines[this.getWho()].pop()
 	}
