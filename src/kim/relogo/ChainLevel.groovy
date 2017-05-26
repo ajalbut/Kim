b824dc6544c5d7314781fa1bb93650b6429f70a7
André <andre.jalbut@gmail.com>
2017-05-26 15:16:18 -0300

    Implementa camada visual para laços de confiança

--- src/kim/relogo/ChainLevel.groovy
+++ src/kim/relogo/ChainLevel.groovy
@@ -16,8 +16,8 @@ class ChainLevel extends ReLogoTurtle {
 	static ALPHA = 0.5
 	static BETA = 1.0
 	static THETA = 0.5
-	static String RULE = 'TRUST'
 
+	def supplyRule
 	def desiredStock = 20.0
 	def currentStock
 	def expectedDemand
@@ -39,6 +39,8 @@ class ChainLevel extends ReLogoTurtle {
 	Map ordersReceivedChecklist = [:]
 	Map trustUpstreams = [:]
 	Map trustDownstreams = [:]
+	def maxTrustUpstreams = 1.0
+	def maxTrustDownstreams = 1.0
 
 	def upstreamLevel
 	def downstreamLevel
@@ -53,7 +55,7 @@ class ChainLevel extends ReLogoTurtle {
 	def setup(x, y, initialStock){
 		setxy(x,y)
 		setShape("square")
-		setColor(white())
+		setColor(yellow())
 		this.currentStock = initialStock
 		this.expectedDemand = 12.0
 		for (ChainLevel upstream in this.upstreamLevel) {
@@ -63,6 +65,8 @@ class ChainLevel extends ReLogoTurtle {
 				this.ordersSentChecklist[upstream.getWho()] = initialOrdersSentChecklist.clone()
 				this.shipmentsReceivedChecklist[upstream.getWho()] = initialShipmentsReceivedChecklist.clone()
 				this.trustUpstreams[upstream.getWho()] = 1.0
+				def route = createLinkFrom(upstream, { hideLink()})
+				route.color = scaleColor(red(), 1.0, 0.0, 1.0)
 			}
 		}
 		for (ChainLevel downstream in this.downstreamLevel) {
@@ -108,7 +112,7 @@ class ChainLevel extends ReLogoTurtle {
 		def totalShipmentsSent = (this.currentStock >= totalOrdersToFulfill) ? totalOrdersToFulfill : this.currentStock
 		for (ChainLevel downstream in this.downstreamLevel) {
 			def shipmentSent
-			if (this.RULE == 'BACKORDER') {
+			if (this.supplyRule == 'BACKORDER') {
 				shipmentSent = totalOrdersToFulfill ? totalShipmentsSent * this.ordersToFulfill[downstream.getWho()] / totalOrdersToFulfill : 0.0
 			} else {
 				shipmentSent = totalTrustDownstreams ? totalShipmentsSent * this.trustDownstreams[downstream.getWho()] / totalTrustDownstreams : 0.0
@@ -130,7 +134,7 @@ class ChainLevel extends ReLogoTurtle {
 			if(upstream.getWho() != this.getWho()) {
 				supplyLine += upstream.lastOrdersToFulfill[this.getWho()]
 				supplyLine += upstream.lastOrderPipelines[this.getWho()].sum()
-				if (this.RULE == 'BACKORDER') {
+				if (this.supplyRule == 'BACKORDER') {
 					if (!upstream.lastOrdersToFulfill[this.getWho()]) {
 						freeUpstreams.push(upstream.getWho())
 					} else {
@@ -158,7 +162,7 @@ class ChainLevel extends ReLogoTurtle {
 			if(upstream.getWho() == this.getWho()) {
 				orderSent = totalOrdersSent
 			} else {
-				if (this.RULE == 'BACKORDER') {
+				if (this.supplyRule == 'BACKORDER') {
 					if(length(freeUpstreams)) {
 						orderSent = (freeUpstreams.contains(upstream.getWho())) ? totalOrdersSent / length(freeUpstreams) : 0.0
 					} else {
@@ -174,32 +178,47 @@ class ChainLevel extends ReLogoTurtle {
 	}
 
 	def updateTrust(){
+		def maxTrustUpstreams = 0.0
 		for (ChainLevel upstream in this.upstreamLevel) {
 			if(upstream.getWho() != this.getWho()) {
 				def orderToCheck = this.ordersSentChecklist[upstream.getWho()].pop()
 				def shipmentReceived = this.shipmentsReceivedChecklist[upstream.getWho()][3]
 				def newEvaluation
+				def updatedTrust = 0.0
 				if (shipmentReceived >= orderToCheck) {
 					newEvaluation = this.RHO * orderToCheck + (1 - this.RHO) * this.trustUpstreams[upstream.getWho()]
-					this.trustUpstreams[upstream.getWho()] = Math.max(newEvaluation, this.trustUpstreams[upstream.getWho()])
+					updatedTrust = Math.max(newEvaluation, this.trustUpstreams[upstream.getWho()])
 				} else {
 					newEvaluation = this.RHO * shipmentReceived + (1 - this.RHO) * this.trustUpstreams[upstream.getWho()]
-					this.trustUpstreams[upstream.getWho()] = Math.min(newEvaluation, this.trustUpstreams[upstream.getWho()])
+					updatedTrust = Math.min(newEvaluation, this.trustUpstreams[upstream.getWho()])
+				}
+				this.trustUpstreams[upstream.getWho()] = updatedTrust
+				if (updatedTrust > maxTrustUpstreams) {
+					maxTrustUpstreams = updatedTrust
 				}
 			}
 		}
+		this.maxTrustUpstreams = maxTrustUpstreams
+
+		def maxTrustDownstreams = 0.0
 		for (ChainLevel downstream in this.downstreamLevel) {
 			def shipmentToCheck = this.shipmentsSentChecklist[downstream.getWho()].pop()
 			def orderReceived = this.ordersReceivedChecklist[downstream.getWho()][2]
 			def newEvaluation
+			def updatedTrust = 0.0
 			if (orderReceived >= shipmentToCheck) {
 				newEvaluation = this.RHO * shipmentToCheck + (1 - this.RHO) * this.trustDownstreams[downstream.getWho()]
-				this.trustDownstreams[downstream.getWho()] = Math.max(newEvaluation, this.trustDownstreams[downstream.getWho()])
+				updatedTrust = Math.max(newEvaluation, this.trustDownstreams[downstream.getWho()])
 			} else {
 				newEvaluation = this.RHO * orderReceived + (1 - this.RHO) * this.trustDownstreams[downstream.getWho()]
-				this.trustDownstreams[downstream.getWho()] = Math.min(newEvaluation, this.trustDownstreams[downstream.getWho()])
+				updatedTrust = Math.min(newEvaluation, this.trustDownstreams[downstream.getWho()])
+			}
+			this.trustDownstreams[downstream.getWho()] = updatedTrust
+			if (updatedTrust > maxTrustDownstreams) {
+				maxTrustDownstreams = updatedTrust
 			}
 		}
+		this.maxTrustDownstreams = maxTrustDownstreams
 	}
 
 	def updateState(){
@@ -223,7 +242,9 @@ class ChainLevel extends ReLogoTurtle {
 	def getCurrentTrustFromUpstreams() {
 		def trust = 0.0
 		for (ChainLevel upstream in this.upstreamLevel) {
-			trust += upstream.trustDownstreams[this.getWho()]
+			if(upstream.getWho() != this.getWho()) {
+				trust += upstream.trustDownstreams[this.getWho()]
+			}
 		}
 		return trust
 	}
@@ -234,5 +255,22 @@ class ChainLevel extends ReLogoTurtle {
 			trust += downstream.trustUpstreams[this.getWho()]
 		}
 		return trust
+	}
+
+	def refreshTrustLinks(visibility) {
+		ask(myOutLinks()){die()}
+		if (visibility == 'upstream') {
+			for (ChainLevel upstream in this.upstreamLevel) {
+				if(upstream.getWho() != this.getWho()) {
+					def route = createLinkTo(upstream)
+					route.color = scaleColor(red(), this.trustUpstreams[upstream.getWho()], 0.0, maxTrustUpstreams)
+				}
+			}
+		} else if (visibility == 'downstream') {
+			for (ChainLevel downstream in this.downstreamLevel) {
+				def route = createLinkTo(downstream)
+				route.color = scaleColor(blue(), this.trustDownstreams[downstream.getWho()], 0.0, maxTrustDownstreams)
+			}
+		}
 	}
 }
